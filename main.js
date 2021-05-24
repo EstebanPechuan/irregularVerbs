0f3e6223b652138a45c4843d1676619a6cce4884
EstebanPechuan <51751504+EstebanPechuan@users.noreply.github.com>
2021-05-24 18:51:06 -0300

    Now works randomly

--- main.js
+++ main.js
@@ -2,35 +2,26 @@ const body = document.querySelector('body')
 const bigBigBox = document.querySelector('.bigBigBox')
 const right = document.querySelector('.right')
 const left = document.querySelector('.left')
-const bigBox = document.querySelectorAll('.bigBox')
+let bigBox = document.querySelectorAll('.bigBox')
+let cantidadCajas = bigBox.length
 
-// let ordenRandom = []
+let ordenRandom = []
 
-// auxRandom = randomNumber()
-// ordenRandom.push(auxRandom)
+for (let i = 0; i <= cantidadCajas-1; i++) {
+    bigBox = document.querySelectorAll('.bigBox')
+    let numRandom = Math.floor(Math.random() * bigBox.length);
 
-// for (i = 0; i < 9; i++) {
-//     auxRandom = randomNumber()
-//     let auxBoolean = true
+    ordenRandom.push(bigBox[numRandom])
+    bigBigBox.removeChild(bigBox[numRandom])
+    console.log(numRandom)
+}
 
-//     while ( auxBoolean ) {
-//         for (let j = 0; j < ordenRandom.length; j++) {
-//             if (auxRandom != ordenRandom[j]) {
-//                 auxBoolean = false
-//             } else {
-//                 auxRandom = randomNumber()
-//             }
-//         }
-//     }
+for (let j = 0; j <= cantidadCajas-1; j++) {
+    bigBigBox.appendChild(ordenRandom[j])
+}
 
-//     ordenRandom.push(auxRandom)
-// }
-
-// console.log(ordenRandom)
-
-// function randomNumber() {
-//     return Math.floor(Math.random() * 10);
-// }
+bigBox = document.querySelectorAll('.bigBox')
+ordenRandom[0].setAttribute('class', 'bigBox active')
 
 let index = 0
 
@@ -69,82 +60,3 @@ function leftButton() {
         left.setAttribute('class', 'displayNone')
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Contenido {
-//     constructor(rotacion) {
-//         this.rotacion = 0;
-//     }
-//     crearCajas() {
-//         let bigBox = document.createElement('div')      
-
-//         bigBox.setAttribute('class', 'bigBox')
-//         bigBox.setAttribute('onclick', 'caja.rotate(this)')
-//         bigBigBox.appendChild(bigBox)
-
-//         for (let i = 0; i < 3; i++) {
-//             let box = document.createElement('div')
-//             box.setAttribute('class', 'box')
-//             bigBox.appendChild(box)
-//         }
-//     }
-
-// }
-
-// let caja = new Contenido()
-// caja.crearCajas()
-
-// let caja2 = new Contenido()
-// caja2.crearCajas()
-
-// function box(hola) {
-//     hola.rotacion -= 120
-//     hola.setAttribute('style', 'transform: scale(0.5) rotateY('+ hola.rotacion +'deg); transition: 0.5s;')
-// }
-
-// function leftButton() {
-//     rotacion += 120
-//     bigBox.setAttribute('style', 'transform: scale(0.5) rotateY('+ rotacion +'deg); transition: 0.5s;')
-// }
-
-// function rightButton() {
-//     rotacion -= 120
-//     bigBox.setAttribute('style', 'transform: scale(0.5) rotateY('+ rotacion +'deg); transition: 0.5s;')
-// }
